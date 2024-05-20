4e21fd78aba60658d0b602d1ca3e2cbe4fcd4910
kimchanho97 <nh0903@pusan.ac.kr>
2024-05-21 01:10:54 +0900

    feat: Add table models

--- app/models.py
+++ app/models.py
@@ -1,13 +1,15 @@
 from . import db
+from datetime import datetime
 
 class User(db.Model):
     __tablename__ = 'User'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     login = db.Column(db.String(255), nullable=False, unique=True)
     nickname = db.Column(db.String(255), nullable=False)
-    avatar_url = db.Column(db.String(255), nullable=True)
+    avatar_url = db.Column(db.String(255), nullable=False)
 
     tokens = db.relationship('Token', backref='User', uselist=False, lazy=True)
+    projects = db.relationship('Project', backref='User', lazy=True)
 
     def __repr__(self):
         return f'<User {self.login}>'
@@ -19,4 +21,59 @@ class Token(db.Model):
 
     def __repr__(self):
         return f'<Token {self.access_token}>'
+
+class Project(db.Model):
+    __tablename__ = 'Project'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    framework = db.Column(db.String(255), nullable=False)
+    port = db.Column(db.Integer, nullable=True)
+    status = db.Column(db.Integer, nullable=False, default=0)
+    autoscaling = db.Column(db.Boolean, nullable=False, default=False)
+    min_replicas = db.Column(db.Integer, nullable=True)
+    max_replicas = db.Column(db.Integer, nullable=True)
+    cpu_threshold = db.Column(db.Integer, nullable=True)
+    domain_url = db.Column(db.String(255), nullable=True)
+    webhook_url = db.Column(db.String(255), nullable=True)
+
+    builds = db.relationship('Build', backref='Project', lazy=True)
+    deployments = db.relationship('Deployment', backref='Project', lazy=True)
+    secrets = db.relationship('Secret', backref='Project', lazy=True)
+
+    def __repr__(self):
+        return f'<Project {self.name}>'
+
+class Build(db.Model):
+    __tablename__ = 'Build'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    project_id = db.Column(db.Integer, db.ForeignKey('Project.id'), nullable=False)
+    build_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    commit_msg = db.Column(db.String(255), nullable=False)
+    image_name = db.Column(db.String(255), nullable=False)
+    image_tag = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return f'<Build {self.id}>'
+
+class Deployment(db.Model):
+    __tablename__ = 'Deployment'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    project_id = db.Column(db.Integer, db.ForeignKey('Project.id'), nullable=False)
+    build_id = db.Column(db.Integer, db.ForeignKey('Build.id'), nullable=False)
+    deploy_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Deployment {self.id}>'
+
+class Secret(db.Model):
+    __tablename__ = 'Secret'
+    # project_id, key로 복합키 설정
+    project_id = db.Column(db.Integer, db.ForeignKey('Project.id'), primary_key=True, nullable=False)
+    key = db.Column(db.String(255), primary_key=True, nullable=False)
+    value = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return f'<Secret {self.key}>'
+
 
