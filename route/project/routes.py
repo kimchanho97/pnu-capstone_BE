138d09303ba6ac24cada73a217cab3ecec35c381
kimchanho97 <nh0903@pusan.ac.kr>
2024-05-25 14:47:23 +0900

    chore: Remove not used api

--- route/project/routes.py
+++ route/project/routes.py
@@ -9,65 +9,19 @@ from .task import triggerArgoWorkflow, deployWithHelm
 projectBlueprint = Blueprint('project', __name__)
 
 
-@projectBlueprint.route('/deploy/event', methods=['POST'])
-def handleHelmWebhook():
+@projectBlueprint.route('/', methods=['GET'])
+def getProjects():
     try:
-        data = request.json
-        buildId = data['buildId']
-        status = data['status']
-
-        build = Build.query.filter_by(id=buildId).first()
-        project = Project.query.filter_by(id=build.project_id).first()
-
-        if status == 'Succeeded':
-            newDeployId = createDeployAndFlush(buildId)
-            project.status = 4  # 배포 완료
-            project.current_deploy_id = newDeployId
-            project.current_build_id = buildId
-        else:
-            project.status = 6  # 배포 실패
-
-        db.session.commit()
-        sendSseMessage(f"{project.user_id}", {'projectId': project.id,
-                                              'status': project.status,
-                                              'currentBuildId': project.current_build_id,
-                                              'currentDeployId': project.current_deploy_id})
-
-        return jsonify({'message': 'Webhook event processed successfully!'}), 200
+        token = request.headers.get('Authorization')
+        token = token.split(' ')[1]
+        userId = getUserIdFromToken(token)
+        responseData = fetchProjects(userId)
+        return make_response(jsonify(responseData), 200)
+    except AuthorizationError as e:
+        return jsonify({'error': str(e)}), 401
     except Exception as e:
-        return jsonify({'error': {'message': 'An error occurred while processing the webhook event.',
-                                  'status': 500}}), 500
+        return jsonify({'error': 'An error occurred while fetching projects.'}), 500
 
-
-@projectBlueprint.route('/build/event', methods=['POST'])
-def handleArgoBuildEvent():
-    try:
-        data = request.json
-        projectId = data['projectId']
-        imageName = data['imageName']
-        imageTag = data['imageTag']
-        commitMsg = data['commitMsg']
-        status = data['status']
-
-        project = Project.query.filter_by(id=projectId).first()
-
-        if status == 'Succeeded':
-            newBuildId = createBuildAndFlush(projectId, commitMsg, imageName, imageTag)
-            project.status = 2  # 빌드 완료
-            project.current_build_id = newBuildId
-        else:
-            project.status = 5  # 빌드 실패
-        db.session.commit()
-
-        sendSseMessage(f"{project.user_id}", {'projectId': projectId,
-                                              'status': project.status,
-                                              'currentBuildId': project.current_build_id,
-                                              'currentDeployId': project.current_deploy_id})
-
-        return make_response(jsonify({'message': 'project build success'}), 200)
-    except Exception as e:
-        return jsonify({'error': {'message': 'An error occurred while building the project.',
-                                  'status': 500}}), 500
 
 @projectBlueprint.route('/<projectId>', methods=['GET'])
 def getProjectDetail(projectId):
@@ -85,18 +39,14 @@ def getProjectDetail(projectId):
                                   'status': 500}}), 500
 
 
-@projectBlueprint.route('/status/<projectId>', methods=['GET'])
-def getProjectStatus(projectId):
+@projectBlueprint.route('/<projectId>', methods=['DELETE'])
+def deleteProject(projectId):
     try:
         token = request.headers.get('Authorization')
         token = token.split(' ')[1]
         getUserIdFromToken(token)
-        project = Project.query.filter_by(id=projectId).first()
-        if project is None:
-            return jsonify({'error': {'message': 'Project not found',
-                                      'status': 404}}), 404
-
-        return jsonify({'status': project.status}), 200
+        deleteProjectById(projectId)
+        return make_response(jsonify({'message': 'Project deleted successfully!'}), 200)
     except AuthorizationError as e:
         return jsonify({'error': {'message': str(e),
                                   'status': 401}}), 401
@@ -153,6 +103,7 @@ def buildProject():
         return jsonify({'error': {'message': 'An error occurred while building the project.',
                                   'status': 500}}), 500
 
+
 @projectBlueprint.route('/deploy', methods=['POST'])
 def deployProject():
     try:
@@ -197,37 +148,6 @@ def deployProject():
                                   'status': 500}}), 500
 
 
-
-@projectBlueprint.route('/<projectId>', methods=['DELETE'])
-def deleteProject(projectId):
-    try:
-        token = request.headers.get('Authorization')
-        token = token.split(' ')[1]
-        getUserIdFromToken(token)
-        deleteProjectById(projectId)
-        return make_response(jsonify({'message': 'Project deleted successfully!'}), 200)
-    except AuthorizationError as e:
-        return jsonify({'error': {'message': str(e),
-                                  'status': 401}}), 401
-    except Exception as e:
-        return jsonify({'error': {'message': 'An error occurred while building the project.',
-                                  'status': 500}}), 500
-
-
-@projectBlueprint.route('/', methods=['GET'])
-def getProjects():
-    try:
-        token = request.headers.get('Authorization')
-        token = token.split(' ')[1]
-        userId = getUserIdFromToken(token)
-        responseData = fetchProjects(userId)
-        return make_response(jsonify(responseData), 200)
-    except AuthorizationError as e:
-        return jsonify({'error': str(e)}), 401
-    except Exception as e:
-        return jsonify({'error': 'An error occurred while fetching projects.'}), 500
-
-
 @projectBlueprint.route('/create', methods=['POST'])
 def createProject():
     try:
@@ -241,5 +161,69 @@ def createProject():
         return jsonify({'error': {'message': str(e),
                                   'status': 401}}), 401
     except Exception as e:
+        return jsonify({'error': {'message': 'An error occurred while creating the project.',
+                                  'status': 500}}), 500
+
+
+@projectBlueprint.route('/build/event', methods=['POST'])
+def handleArgoBuildEvent():
+    try:
+        data = request.json
+        projectId = data['projectId']
+        imageName = data['imageName']
+        imageTag = data['imageTag']
+        commitMsg = data['commitMsg']
+        status = data['status']
+
+        project = Project.query.filter_by(id=projectId).first()
+
+        if status == 'Succeeded':
+            newBuildId = createBuildAndFlush(projectId, commitMsg, imageName, imageTag)
+            project.status = 2  # 빌드 완료
+            project.current_build_id = newBuildId
+        else:
+            project.status = 5  # 빌드 실패
+        db.session.commit()
+
+        sendSseMessage(f"{project.user_id}", {'projectId': projectId,
+                                              'status': project.status,
+                                              'currentBuildId': project.current_build_id,
+                                              'currentDeployId': project.current_deploy_id})
+
+        return make_response(jsonify({'message': 'project build success'}), 200)
+    except Exception as e:
         return jsonify({'error': {'message': 'An error occurred while building the project.',
+                                  'status': 500}}), 500
+
+
+@projectBlueprint.route('/deploy/event', methods=['POST'])
+def handleHelmWebhook():
+    try:
+        data = request.json
+        buildId = data['buildId']
+        status = data['status']
+
+        build = Build.query.filter_by(id=buildId).first()
+        project = Project.query.filter_by(id=build.project_id).first()
+
+        if status == 'Succeeded':
+            newDeployId = createDeployAndFlush(buildId)
+            project.status = 4  # 배포 완료
+            project.current_deploy_id = newDeployId
+            project.current_build_id = buildId
+
+            # 서브도메인 할당 및 DB에 저장
+
+        else:
+            project.status = 6  # 배포 실패
+
+        db.session.commit()
+        sendSseMessage(f"{project.user_id}", {'projectId': project.id,
+                                              'status': project.status,
+                                              'currentBuildId': project.current_build_id,
+                                              'currentDeployId': project.current_deploy_id})
+
+        return jsonify({'message': 'Webhook event processed successfully!'}), 200
+    except Exception as e:
+        return jsonify({'error': {'message': 'An error occurred while processing the webhook event.',
                                   'status': 500}}), 500
